1d92939d0ad2249d6112513f9f7197dc5bdb2a19
Plaga <benitopab@gmail.com>
2023-04-22 00:38:46 -0300

    filterbyName Function was refactored, 22-04-23

--- src/utils/FilterByName.ts
+++ src/utils/FilterByName.ts
@@ -1,19 +1,9 @@
 import type PokemonInterface from "../interfaces/PokemonInterface";
 
 export const FilterByName = (elem: PokemonInterface[], keyName: string): PokemonInterface[] => {
-    const final = [];
-    if (keyName !== '') {
-        for (let i = 0; i < elem.length; i++) {
-            if(elem[i].name.indexOf(keyName) !== -1) {
-                if (elem[i].name[0] === keyName[0]) {
-                    final.push(elem[i])
-                    continue;
-                }
-            }else{
-                continue;
-            }
-        }
-        return final;
-    }
-    return elem;
+    let res: PokemonInterface[] = [];
+
+    res = elem.filter(elem => elem.name.startsWith(keyName))
+
+    return res;
 }
